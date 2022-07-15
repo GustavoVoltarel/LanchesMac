e618b22445dd9ba3f2ccf07e21f2a4f4c07543d1
Gustavo Voltarel <g.voltarel95@gmail.com>
2022-07-15 11:48:03 -0300

    Criação de ViewModels para o projeto

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lanchesListViewModel);
+        }
+    }
+}
