c65a0d2397fc3463101849f42a0284efd58cea14
Gustavo Voltarel <g.voltarel95@gmail.com>
2022-07-17 10:00:32 -0300

    Criação do carrinho de compra

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -23,6 +24,12 @@ public class Startup
 
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+        //Scoped pois caso 2 clientes façam um request ao mesmo tempo, terão carrinhos diferentes, pois são requests diferentes(atualizado a cada request)
+        services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        services.AddMemoryCache();
+        services.AddSession();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +50,7 @@ public class Startup
         app.UseStaticFiles();
         app.UseRouting();
         app.UseAuthorization();
+        app.UseSession();
 
         app.UseEndpoints(endpoints =>
         {
