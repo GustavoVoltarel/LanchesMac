ac342f433e4e6f6fca6d83d86b839c03375104be
Gustavo Voltarel <g.voltarel95@gmail.com>
2022-07-23 20:39:17 -0300

    Implementação de upload e exclusão de imagens na area Admin

---
+++ LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,99 @@
+﻿using LanchesMac.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminImagensController : Controller
+    {
+        private readonly ConfigurationImagens _myConfig;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AdminImagensController(IWebHostEnvironment webHostEnvironment, IOptions<ConfigurationImagens> myConfig)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _myConfig = myConfig.Value;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+            {
+                ViewData["Erro"] = "Erro: Arquivo(s) não selecionado(s)";
+                return View(ViewData);
+            }
+
+            if (files.Count > 10)
+            {
+                ViewData["Erro"] = "Erro: Quantidade de arquivos excedeu o limite. Limite maximo de 10 arquivos por vez";
+                return View(ViewData);
+            }
+
+            long size = files.Sum(f => f.Length);
+
+            var filePathName = new List<string>();
+
+            var filePath = Path.Combine(_webHostEnvironment.WebRootPath, _myConfig.NomePastaImagensProdutos);
+
+            foreach (var file in files)
+            {
+                if (file.FileName.Contains(".jpg") || file.FileName.Contains(".gif") || file.FileName.Contains(".png"))
+                {
+                    var fileNameWithPath = string.Concat(filePath, "\\", file.FileName);
+                    filePathName.Add(fileNameWithPath);
+
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            ViewData["Resultado"] = $"{files.Count} arquivos foram enviados ao servidor, " + $"com tamanho total de {size} bytes";
+            ViewBag.Arquivos = filePathName;
+
+            return View(ViewData);
+        }
+
+        public IActionResult GetImagens()
+        {
+            FileManagerModel model = new FileManagerModel();
+            var userImagesPath = Path.Combine(_webHostEnvironment.WebRootPath, _myConfig.NomePastaImagensProdutos);
+
+            DirectoryInfo dir = new DirectoryInfo(userImagesPath);
+            FileInfo[] files = dir.GetFiles();
+
+            model.PathImagesProduto = _myConfig.NomePastaImagensProdutos;
+
+            if (files.Length == 0)
+            {
+                ViewData["Erro"] = $"Nenhum arquivo encontrado na pasta {userImagesPath}";
+            }
+
+            model.Files = files;
+
+            return View(model);
+        }
+
+        public IActionResult Deletefile(string fileName)
+        {
+            string _imagemDeletar = Path.Combine(_webHostEnvironment.WebRootPath, _myConfig.NomePastaImagensProdutos + "\\", fileName);
+
+            if ((System.IO.File.Exists(_imagemDeletar)))
+            {
+                System.IO.File.Delete(_imagemDeletar);
+                ViewData["Deletado"] = $"Arquivo(s) {_imagemDeletar} deletado com sucesso";
+            }
+
+            return View("Index");
+        }
+    }
+}
