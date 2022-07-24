e8b9447067e48424d7a3cc89cfb394111f2b5696
Gustavo Voltarel <g.voltarel95@gmail.com>
2022-07-24 12:25:44 -0300

    Geração de Grafico de vendas para a area Admin com o Google Charts

---
+++ LanchesMac/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -0,0 +1,43 @@
+﻿using LanchesMac.Areas.Admin.Servicos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminGraficoController : Controller
+    {
+        private readonly GraficoVendasService _graficoVendasService;
+
+        public AdminGraficoController(GraficoVendasService graficoVendasService)
+        {
+            _graficoVendasService = graficoVendasService ?? throw
+                new ArgumentNullException(nameof(graficoVendasService));
+        }
+
+        public JsonResult VendasLanches(int dias)
+        {
+            var lanchesVendasTotais = _graficoVendasService.GetVendasLanches(dias);
+            return Json(lanchesVendasTotais);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult VendaMensal()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult VendaSemanal()
+        {
+            return View();
+        }
+    }
+}
